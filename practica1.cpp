04d520a45dab1d8ed37e46ebe39d5ce7f94e1b9c
dngxue <paolareyes802@gmail.com>
2024-03-11 21:16:06 -0600

    comentarios

--- practica1.cpp
+++ practica1.cpp
@@ -26,64 +26,72 @@ void agregarFinales(const string& simbolo){
     estadosFinales.push_back(simbolo);
 }
 
-void imprimir() {
-    cout << "\n\033[96mAlfabeto:\033[0m";
-    for(int i=0; i<alfabeto.size(); i++) {
-        cout << " " << alfabeto.at(i);
-    }
+// void imprimir() {
+//     cout << "\n\033[96mAlfabeto:\033[0m";
+//     for(int i=0; i<alfabeto.size(); i++) {
+//         cout << " " << alfabeto.at(i);
+//     }
 
-    cout << "\n\033[93mEstado Inicial:\033[0m " << estadoInicial;
+//     cout << "\n\033[93mEstado Inicial:\033[0m " << estadoInicial;
 
-    cout << "\n\033[93mEstados:\033[0m";
-    for(int i=0; i<estados.size(); i++) {
-        cout << " " << estados.at(i);
-    }
+//     cout << "\n\033[93mEstados:\033[0m";
+//     for(int i=0; i<estados.size(); i++) {
+//         cout << " " << estados.at(i);
+//     }
 
-    cout << "\n\033[93mEstados Finales:\033[0m";
-    for(int i=0; i<estadosFinales.size(); i++) {
-        cout << " " << estadosFinales.at(i);
-    }
-
-    // cout << "\n\033[96mTabla de transiciones:\n\033[0m";
-    // for(int i=0; i<transiciones.size(); i++) {
-    //     for(int j=0; j<transiciones.at(0).size(); j++) {
-    //         cout << transiciones.at(i).at(j) << " ";
-    //     }
-    //     cout << "\n";
-    // }
-}
-
+//     cout << "\n\033[93mEstados Finales:\033[0m";
+//     for(int i=0; i<estadosFinales.size(); i++) {
+//         cout << " " << estadosFinales.at(i);
+//     }
+// }
+// para obtener el índice de la fila
 size_t getFila(string estadoActual) {
+    // busca el estado en el vector
     auto estado = find(estados.begin(), estados.end(), estadoActual);
+    // calcula la distancia (el índice)
+    // entre el inicio del vector y el estado
     return distance(estados.begin(), estado);
 }
-
+// para obtener el índice de la columna
 size_t getColumna(char c) {
     auto caracter = find(alfabeto.begin(), alfabeto.end(), c);
     return distance(alfabeto.begin(), caracter);
 }
 
 bool automata(){
-    // Encontrar el índice de un estado
+    // Primero se busca el índice del estado inicial (fila)
     size_t estadoActual = getFila(estadoInicial);
-    //cout << "El elemento " << estadoInicial << " se encuentra en el índice " << estadoActual << "\tEstado: "<< estados.at(estadoActual) <<endl;
 
+    // itera sobre cada símbolo de la cadena de entrada
     for(int i=0; i<cadena.size(); i++){
+        // busca el símbolo en la posición i de la cadena a través
+        // de nuestro vector que contiene el alfabeto 
         if(find(alfabeto.begin(), alfabeto.end(), cadena.at(i)) != alfabeto.end()){
+            // se busca el estado al que transita en la fila de nuestro 
+            // estado actual y en la columna en donde se escuentra el 
+            // símbolo dentro de nuestro alfabeto, se guarda en una variable
+            // ese estado
             string s = transiciones.at(estadoActual).at(getColumna(cadena.at(i)));
+            // se actualiza el valor del índice de la fila de
+            // nuestro estado actual
             estadoActual = getFila(s);
         }else{
+            // si no encuentra el símbolo dentro del alfabeto
             cout << "\n\033[91mCadena NO valida\033[0m" << endl;
             return false;
         }
     }
 
+    // Una vez que termina de evaluar la cadena, iteramos a lo
+    // largo de nuestro vector de estados finales para
+    // verificar si el estado actual es uno de los estados finales
     for(int i=0; i<estadosFinales.size(); i++){
         if(estados.at(estadoActual) == estadosFinales.at(i)) {
             cout << "\n\033[92mCadena aceptada\033[0m";
             return true;
         }
     }
+    // si no es parte de los estados finales
     cout << "\n\033[91mCadena NO aceptada\033[0m";
     return false;
 }
@@ -103,9 +111,12 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // para almacenar cada línea leída del archivo
     string linea;
+    // para saber el número de línea actual
     int num_linea = 1;
 
+    // lee cada línea del archivo
     while(getline(archivo,linea)){
 
         if(num_linea == 1){ // alfabeto
@@ -141,7 +152,7 @@ int main(int argc, char *argv[]) {
         num_linea++;
     }
 
-    imprimir();
+    // imprimir();
     automata();
 
     cout << "\n";
